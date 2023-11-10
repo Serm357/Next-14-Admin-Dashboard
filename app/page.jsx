99523ab5a2be5a26dea4951a5bfaa71a5c639642
Serm357 <kilangalilasalum@gmail.com>
2023-11-10 09:30:38 +0300

    testing mongodb

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,42 @@
 // TODO
-const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+import { MongoClient } from "mongodb";
 
-export default Homepage
+const Homepage = () => {
+  // Replace the uri string with your MongoDB deployment's connection string.
+  const uri = process.env.MONGO;
+
+  // Create a new client and connect to MongoDB
+  const client = new MongoClient(uri);
+
+  async function run() {
+    try {
+      // Connect to the "insertDB" database and access its "haiku" collection
+      const database = client.db("insertDB");
+      const haiku = database.collection("haiku");
+
+      // Create a document to insert
+      const doc = {
+        title: "Record of a Shriveled Datum",
+        content: "No bytes, no problem. Just insert a document, in MongoDB",
+      };
+      // Insert the defined document into the "haiku" collection
+      const result = await haiku.insertOne(doc);
+
+      // Print the ID of the inserted document
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+      // Close the MongoDB client connection
+      await client.close();
+    }
+  }
+  // Run the function and handle any errors
+  // run().catch(console.dir);
+
+  return (
+    <section>
+      <button onClick={run}>add</button>
+    </section>
+  );
+};
+
+export default Homepage;
