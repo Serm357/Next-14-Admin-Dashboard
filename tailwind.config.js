34d09bf397c5303d19515382e62e95cdbbd678ef
Serm357 <kilangalilasalum@gmail.com>
2023-11-10 11:52:42 +0300

    fixed mongo added tailwind styling hompage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export const content = ["./app/**/*.{js,ts,jsx,tsx,mdx}"];
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#5542F6",
+      highlight: "#eae8fb",
+      bgGray: "#fbfafd",
+    },
+  },
+};
+export const variants = {
+  extend: {},
+};
+// export const darkMode = "class";
+export const plugins = [];
